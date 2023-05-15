1bdfefb99feb94fe9432ee2b239ad52cf8d6860e
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2023-05-15 13:40:07 +0100

    First successful Syntax response from API, its not pretty but works

--- my-project/README.md
+++ my-project/README.md
@@ -2,6 +2,8 @@ Step 1: Connect to API Using POSTMAN with successful response ✅
 Step 2:Begin the build , Establish simple connecting using JSON Endpoint 
 "https://webservices.data-8.co.uk/EmailValidation/IsValid.json" and return Syntax level
 
+Step 2.1: Define states, email, errors, submission and response
+
 
 
 Tech Stack:
--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,13 +1,66 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import axios from 'axios'
 
 const App = () => {
+  // Define state variables
+  const [email, setEmail] = useState('');
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [responseDetails, setResponseDetails] = useState(null);
+
+  // In practice would put in env
+  const apiKey = "NHWE-QAUR-DUI4-S3S6";
+
+  // POST request to an email validation service
+  const validateEmail = (email) => {
+    const url = "https://webservices.data-8.co.uk/EmailValidation/IsValid.json";
+    const data = {
+      "username": `apikey-${apiKey}`,
+      "password": "",
+      "email": email,
+      "level": "Syntax",
+      "options": {}
+    };
+
+    // axios.post returns a promise that resolves to the response data
+    return axios.post(url, data)
+      .then((response) => response.data)
+      .catch((error) => {
+        console.error("Error", error);
+      });
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
+
+    if (isSubmitting) {
+      return;
+    }
+
+    setIsSubmitting(true); 
+
+    // Call validateEmail and handle its promise
+    validateEmail(email)
+      .then((response) => {
+        if (response.Result === "Valid") {
+          setResponseDetails(JSON.stringify(response, null, 2));  //If valid return response
+        } else {
+          setErrorMessage('Please enter a valid email address');
+        }
+      })
+      .finally(() => setIsSubmitting(false));
+  }
+
   return (
-  <div>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  </div>
-  )
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+        {errorMessage && <p>{errorMessage}</p>}
+        <button type="submit">Submit</button>
+        {responseDetails && <textarea>{responseDetails}</textarea>}
+      </form>
+    </div>
+  );
 }
+
 export default App;
