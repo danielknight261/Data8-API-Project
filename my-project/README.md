9bd82c40890af83e1fb2a083e07e393fb33a39ce
danielknight261 <100095189+danielknight261@users.noreply.github.com>
2023-05-15 14:04:06 +0100

    added all 4 levels with successful responses add further styling viable mvp

--- my-project/README.md
+++ my-project/README.md
@@ -1,8 +1,11 @@
 Step 1: Connect to API Using POSTMAN with successful response ✅
 Step 2:Begin the build , Establish simple connecting using JSON Endpoint 
-"https://webservices.data-8.co.uk/EmailValidation/IsValid.json" and return Syntax level
+"https://webservices.data-8.co.uk/EmailValidation/IsValid.json" and return Syntax level ✅
 
-Step 2.1: Define states, email, errors, submission and response
+Step 2.1: Define states, email, errors, submission and response ✅
+Step 3: Include all 4 of the search option (update state, define array and update data object) ✅
+Step 4:make the input and response layout cleaner "textarea" is not correct choice. ✅
+
 
 
 
--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,66 +1,101 @@
-import React, { useState } from 'react';
-import axios from 'axios'
+import React, { useState } from "react";
+import axios from "axios";
 
 const App = () => {
   // Define state variables
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
   const [errorMessage, setErrorMessage] = useState(null);
+  const [level, setLevel] = useState("Syntax");
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [responseDetails, setResponseDetails] = useState(null);
 
   // In practice would put in env
   const apiKey = "NHWE-QAUR-DUI4-S3S6";
+  const levels = ["Syntax", "MX", "Server", "Address"];
 
   // POST request to an email validation service
-  const validateEmail = (email) => {
+  const validateEmail = (email, level) => {
     const url = "https://webservices.data-8.co.uk/EmailValidation/IsValid.json";
     const data = {
-      "username": `apikey-${apiKey}`,
-      "password": "",
-      "email": email,
-      "level": "Syntax",
-      "options": {}
+      username: `apikey-${apiKey}`,
+      password: "",
+      email: email,
+      level: level,
+      options: {},
     };
 
     // axios.post returns a promise that resolves to the response data
-    return axios.post(url, data)
+    return axios
+      .post(url, data)
       .then((response) => response.data)
       .catch((error) => {
         console.error("Error", error);
       });
-  }
+  };
 
   const handleSubmit = (event) => {
-    event.preventDefault(); 
+    event.preventDefault();
 
     if (isSubmitting) {
       return;
     }
 
-    setIsSubmitting(true); 
+    setIsSubmitting(true);
 
     // Call validateEmail and handle its promise
-    validateEmail(email)
+    validateEmail(email, level)
       .then((response) => {
         if (response.Result === "Valid") {
-          setResponseDetails(JSON.stringify(response, null, 2));  //If valid return response
+          setResponseDetails(JSON.stringify(response, null, 2)); //If valid return response
         } else {
-          setErrorMessage('Please enter a valid email address');
+          setErrorMessage("Please enter a valid email address");
         }
       })
       .finally(() => setIsSubmitting(false));
-  }
+  };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-        {errorMessage && <p>{errorMessage}</p>}
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-300">
+      <form
+        className="p-6 bg-white rounded shadow-md w-80 relative"
+        onSubmit={handleSubmit}
+      >
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Email:
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {/* Message below input if email if invalid */}
+          {errorMessage && <p>{errorMessage}</p>}
+        </div>
+
+        <div>
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Level
+          </label>
+          <select
+            value={level}
+            onChange={(e) => setLevel(e.target.value)}
+            className="cursor-pointer shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          >
+            <option value="Syntax">Syntax</option>
+            <option value="MX">MX</option>
+            <option value="Server">Server</option>
+            <option value="Address">Address</option>
+          </select>
+        </div>
+
+        
         <button type="submit">Submit</button>
-        {responseDetails && <textarea>{responseDetails}</textarea>}
+        {responseDetails && <pre>{responseDetails}</pre>}
       </form>
     </div>
   );
-}
+};
 
 export default App;
